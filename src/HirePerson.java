d13b7dda759b3b3518654b33f395319cd42bd9d7
Aaron <panyitao1997@hotmail.com>
2017-04-23 17:23:37 +0800

    The original version finished in class. Trying to finish some more function.

---
+++ src/HirePerson.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class HirePerson extends JFrame implements ActionListener{
+	/**
+	 * @param args
+	 */
+
+	JDialog addInfoDialog = new JDialog();
+	//JFrame addInfoDialog = new JFrame();
+	
+	public HirePerson(){
+		this.setTitle("求租人信息设置");
+		this.setVisible(true);
+		this.setBounds(200, 200, 800, 400);
+		
+		JPanel operatePanel = new JPanel();
+		operatePanel.setBorder(BorderFactory.createTitledBorder("求租人信息操作"));
+		JButton addButton, changeButton, deleteButton, quitButton;
+		addButton = new JButton("添加");
+		changeButton = new JButton("修改");
+		deleteButton = new JButton("删除");
+		quitButton = new JButton("退出");
+		operatePanel.add(addButton);
+		operatePanel.add(changeButton);
+		operatePanel.add(deleteButton);
+		operatePanel.add(quitButton);
+		this.add(operatePanel, BorderLayout.NORTH);
+		
+		JPanel infoPanel = new JPanel();
+		JLabel tempLabel = new JLabel("建设中，敬请期待！");
+		infoPanel.setBorder(BorderFactory.createTitledBorder("求租人信息列表"));
+		infoPanel.add(tempLabel);
+		this.add(infoPanel, BorderLayout.CENTER);
+		
+		addButton.addActionListener(this);	// 为 addButton 添加时间监听器，由当前窗口 (this) 进行监听
+		quitButton.addActionListener(this);
+		changeButton.addActionListener(this);
+		deleteButton.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if (e.getActionCommand().equals("添加")){		// 获取触发事件的事件源的文本，若相同，则弹出窗体
+			HirePersonAddDialog newHirePerson = new HirePersonAddDialog();
+		}
+		if (e.getActionCommand().equals("退出")){		// 获取触发事件的事件源的文本，若相同，则弹出窗体
+			System.exit(0);
+		}
+		if (e.getActionCommand().equals("修改")){		// 获取触发事件的事件源的文本，若相同，则弹出窗体
+			JDialog changeDialog = new JDialog();
+			changeDialog.setVisible(true);
+			changeDialog.setTitle("修改求租人信息");
+			JLabel tempLabel = new JLabel("建设中，敬请期待！");
+			changeDialog.add(tempLabel);
+			changeDialog.setBounds(200, 200, 400, 400);
+		}
+		if (e.getActionCommand().equals("删除")){		// 获取触发事件的事件源的文本，若相同，则弹出窗体
+			JDialog deleteDialog = new JDialog();
+			deleteDialog.setVisible(true);
+			deleteDialog.setTitle("删除求租人信息");
+			deleteDialog.setLayout(new GridLayout(2, 2));
+			deleteDialog.setBounds(200, 200, 400, 70);
+			
+			JLabel delete = new JLabel("  输入您要删除的求租人编号");
+			JTextField deleteNum = new JTextField(15);
+			deleteDialog.add(delete);
+			deleteDialog.add(deleteNum);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HirePerson hirePerson = new HirePerson();
+		//hirePerson.HirePersonAddDialog();
+	}
+
+}
---
+++ src/HirePersonAddDialog.java
@@ -0,0 +1,155 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+
+public class HirePersonAddDialog implements ActionListener{
+
+	/**
+	 * @param args
+	 */
+	JDialog addInfoDialog;
+	JPanel addInfoPanel;
+	JPanel addOperatePanel;
+	JPanel p1, p2, p3, p4, p5, p6, p7, p8;	// No time to change these into a array, so just use them this way
+	JTextField hirePersonValue, userNameValue, IDValue, phoneNumber, mailAddress, cellNumber, QQNumber;
+	JRadioButton male, female;
+	ButtonGroup sexChoice;
+	
+	public HirePersonAddDialog(){
+		addInfoDialog = new JDialog();
+		addInfoDialog.setBounds(200, 200, 400, 400);
+		addInfoDialog.setTitle("添加求租人信息");
+		addInfoDialog.setVisible(true);
+		
+		addInfoPanel = new JPanel();
+		addInfoPanel.setBorder(BorderFactory.createTitledBorder("添加求租人信息"));
+		addInfoPanel.setLayout(new GridLayout(8, 1));
+		
+		JLabel hirePersonNo = new JLabel("求租人编号");
+		hirePersonValue = new JTextField(15);
+		p1 = new JPanel();
+		p1.add(hirePersonNo);
+		p1.add(hirePersonValue);
+		
+		JLabel userName = new JLabel("姓名             ", JLabel.RIGHT);
+		userNameValue = new JTextField(15);
+		p2 = new JPanel();
+		p2.add(userName);
+		p2.add(userNameValue);
+		
+		JLabel sex = new JLabel("性别             ", JLabel.RIGHT);
+		male = new JRadioButton("男");
+		female = new JRadioButton("女");
+		sexChoice = new ButtonGroup();
+		sexChoice.add(male);
+		sexChoice.add(female);
+		p3 = new JPanel();
+		p3.add(sex);
+		p3.add(male);
+		p3.add(female);
+		
+		JLabel ID = new JLabel("身份证号码");
+		IDValue = new JTextField(15);
+		p4 = new JPanel();
+		p4.add(ID);
+		p4.add(IDValue);
+		
+		JLabel phone = new JLabel("家庭电话    ");
+		phoneNumber = new JTextField(15);
+		p5 = new JPanel();
+		p5.add(phone);
+		p5.add(phoneNumber);
+		
+		JLabel mail = new JLabel("邮箱            ", JLabel.RIGHT);
+		mailAddress = new JTextField(15);
+		p6 = new JPanel();
+		p6.add(mail);
+		p6.add(mailAddress);
+		
+		JLabel cell = new JLabel("手机             ", JLabel.RIGHT);
+		cellNumber = new JTextField(15);
+		p7 = new JPanel();
+		p7.add(cell);
+		p7.add(cellNumber);
+		
+		JLabel QQ = new JLabel("QQ               ", JLabel.RIGHT);
+		QQNumber = new JTextField(15);
+		p8 = new JPanel();
+		p8.add(QQ);
+		p8.add(QQNumber);
+		
+		addInfoPanel.add(p1);
+		addInfoPanel.add(p2);
+		addInfoPanel.add(p3);
+		addInfoPanel.add(p4);
+		addInfoPanel.add(p5);
+		addInfoPanel.add(p6);
+		addInfoPanel.add(p7);
+		addInfoPanel.add(p8);
+		
+		addOperatePanel = new JPanel();
+		JButton bt1 = new JButton("确定");
+		JButton bt2 = new JButton("重置");
+		addOperatePanel.add(bt1);
+		addOperatePanel.add(bt2);
+		
+		bt1.addActionListener(this);
+		bt2.addActionListener(this);
+		
+		addInfoDialog.add(addInfoPanel, BorderLayout.CENTER);
+		addInfoDialog.add(addOperatePanel, BorderLayout.SOUTH);
+		addInfoDialog.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if (e.getActionCommand().equals("重置")){		// 获取触发事件的事件源的文本
+			hirePersonValue.setText("");
+			userNameValue.setText("");
+			male.setSelected(true);
+			IDValue.setText("");
+			phoneNumber.setText("");
+			mailAddress.setText("");
+			cellNumber.setText("");
+			QQNumber.setText("");
+		}
+		if (e.getActionCommand().equals("确定")){		// 获取触发事件的事件源的文本
+			JDialog temp = new JDialog();
+			temp.setTitle("添加成功！");
+			temp.setBounds(400, 400, 300, 100);
+			temp.setVisible(true);
+			
+			JLabel tempLabel = new JLabel("添加成功");
+			temp.add(tempLabel, BorderLayout.CENTER);
+			
+			JButton tempOKButton = new JButton("确认");
+			tempOKButton.addActionListener(this);
+			temp.add(tempOKButton, BorderLayout.SOUTH);
+			
+			addInfoDialog.dispose();
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HirePersonAddDialog newDialog = new HirePersonAddDialog();
+	}
+}
+
+class AddSuccess implements ActionListener{
+	JDialog addSuccess = new JDialog();
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+}
